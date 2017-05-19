c870a97cb43ebc7d816160c2a9e8557aa51a6275
gwilym <gwilym.johnston@isotoma.com>
2017-05-19 14:47:10 +0100

    add propTypes and convert to stateless component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from 'react-toolbox/lib/button'
 
 import Controls from '../components/Controls';
@@ -11,37 +12,51 @@ import { SQUARE_SIZE } from '../const';
 import 'normalize.css';
 import styles from '../styles/App.css';
 
-class App extends Component {
-  render() {
-    const carpetSize = {
-      width: `${this.props.board.width * SQUARE_SIZE }px`,
-      height: `${this.props.board.height * SQUARE_SIZE }px`,
-    };
-    return (
-      <div className={styles.app}>
-        <div className={styles.header}>
-          <h1>Hoover Game</h1>
-          <Button label="Settings" raised accent onClick={this.props.toggleDrawer} />
-        </div>
-        <div className={styles.gameContainer}>
-          <div className={styles.score}>{this.props.dirt.length} dirt spots left!</div>
-          <div className={styles.carpet} style={carpetSize}>
-            <Hoover x={this.props.hoover.x} y={this.props.hoover.y} />
-            {
-              this.props.dirt.map((dirt, i) =>
-                <Dirt key={i} x={dirt.x} y={dirt.y} />
-              )
-            }
-          </div>
-          <Controls moveHoover={this.props.moveHoover} />
-        </div>
-        <Settings
-          handleToggle={this.props.toggleDrawer}
-          active={this.props.drawer}
-        />
+const App = ({hoover, drawer, dirt, board, toggleDrawer, moveHoover}) => {
+  const carpetSize = {
+    width: `${board.width * SQUARE_SIZE }px`,
+    height: `${board.height * SQUARE_SIZE }px`,
+  };
+  return (
+    <div className={styles.app}>
+      <div className={styles.header}>
+        <h1>Hoover Game</h1>
+        <Button label="Settings" raised accent onClick={toggleDrawer} />
       </div>
-    );
-  }
+      <div className={styles.gameContainer}>
+        <div className={styles.score}>{dirt.length} dirt spots left!</div>
+        <div className={styles.carpet} style={carpetSize}>
+          <Hoover x={hoover.x} y={hoover.y} />
+          {
+            dirt.map((dirt, i) =>
+              <Dirt key={i} x={dirt.x} y={dirt.y} />
+            )
+          }
+        </div>
+        <Controls moveHoover={moveHoover} />
+      </div>
+      <Settings
+        handleToggle={toggleDrawer}
+        active={drawer}
+      />
+    </div>
+  );
 }
+
+App.propTypes = {
+  board: PropTypes.shape({
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+  }),
+  dirt: PropTypes.arrayOf(
+      PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+      }),
+  ).isRequired,
+  toggleDrawer: PropTypes.func.isRequired,
+  drawer: PropTypes.bool.isRequired,
+  moveHoover: PropTypes.func.isRequired,
+};
 
 export default App;
